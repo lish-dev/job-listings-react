fb5092610cf5a512213ace66f0ba73f54e93648f
Miss <emesquitta73@gmail.com>
2021-04-09 01:10:52 +0100

    completed site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import data from "./assets/data.json";
+import JobBoard from "./components/JobBoard";
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+  const [filters, setFilters] = useState([]);
+
+  useEffect(() => setJobs(data), []);
+
+  //when the user selects a word for the filtering option
+  const filterFunc = ({ role, level, tools, languages }) => {
+    if (filters.length === 0) {
+      return true;
+    }
+
+    const tags = [role, level];
+
+    if (languages) {
+      tags.push(...languages);
+    }
+    if (tools) {
+      tags.push(...tools);
+    }
+    //checking array tag within the filter, if no tags are selected then all data should be visible
+    return filters.every(filter => tags.includes(filter));
+  };
+
+  // this is to preventing the tag to be re-added each time it is clicked
+  const handleTagClick = (tag) => {
+    if (filters.includes(tag)) return;
+
+    setFilters([...filters, tag]);
+  };
+
+  // if the any item is left in the filter bar this can be clicked on by the user to be removed
+  const handleFilterClick = (passedFilter) => {
+    setFilters(filters.filter((f) => f !== passedFilter));
+  };
+// when the user clicks the 'x' in the search bar this clears all words that were selected previously for filtering
+  const clearFilters = () => {
+    setFilters([]);
+  }
+
+  const filteredJobs = jobs.filter(filterFunc);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="bg-teal-500 mb-12">
+        <img src="/images/bg-header-desktop.svg" alt="bg" />
       </header>
-    </div>
+      <div className="container m-auto">
+      {filters.length > 0 && (
+        
+        <div className={"flex bg-white shadow-md -my-20  mb-16 mx-10 p-6 rounded z-10 relative"}>
+          {filters.map((filter) => (
+            <span
+              className="cursor-pointer mr-4 mb-4 rounded font-bold text-teal-500 bg-teal-100 p-2 lg:mb-0"
+              onClick={() => handleFilterClick(filter)}
+            >
+              x {filter}
+            </span>
+          ))}         
+        <button onClick={clearFilters} className="font-bold text-grey-700 ml-auto">Clear</button>
+        </div>
+        
+      )}
+
+      {jobs.length === 0 ? (
+        <p>Jobs are fetching..</p>
+      ) : (
+        filteredJobs.map((job) => (
+          <JobBoard job={job} key={job.id} handleTagClick={handleTagClick} />
+        ))
+      )}
+      </div>
+    </>
   );
 }
 
